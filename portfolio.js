c0a262a6d128cd375c4c46f440fc3a7038f91c25
Nguyen Huu Phat <huuphat1908@gmail.com>
2021-05-13 18:48:51 +0700

    Modify carousel

--- portfolio.js
+++ portfolio.js
@@ -1,65 +1,81 @@
-function ScrollOffsetTop(){
+function ScrollOffsetTop() {
     let navLink = $('.navbar-nav > .nav-item > .nav-link');
-    navLink.click(function(){
-        let section =  $(this).attr('href');
-        $('html, body').animate({scrollTop: $(section).offset().top - 69}, 200);
+    navLink.click(function () {
+        let section = $(this).attr('href');
+        $('html, body').animate({ scrollTop: $(section).offset().top - 69 }, 200);
     });
 }
 
-function StickyNav(){
-    $(window).scroll(function(){
+function StickyNav() {
+    $(window).scroll(function () {
         let header = $('.header');
         let fakeHeaderNav = $('#fake-header-nav');
         let bannerOffsetTop = $('#home-section').offset().top;
         let heighOfBanner = $('#home-section').outerHeight();
-        if ($(window).scrollTop() > 70){
+        if ($(window).scrollTop() > 70) {
             header.addClass('header_scroll');
-            if ($(window).width() < 768){
+            if ($(window).width() < 768) {
                 fakeHeaderNav.css('height', '69');
             }
         }
-        else{
+        else {
             header.removeClass('header_scroll');
             fakeHeaderNav.css('height', '0');
         }
-        if ($(window).scrollTop() > (bannerOffsetTop + heighOfBanner - header.outerHeight() - 1)){
+        if ($(window).scrollTop() > (bannerOffsetTop + heighOfBanner - header.outerHeight() - 1)) {
             header.addClass('header_NotOnTop');
         }
-        else{
+        else {
             header.removeClass('header_NotOnTop');
         }
     });
 }
 
-function AnimationDownloadCVButton(){
+function AnimationDownloadCVButton() {
     let button = $('#home-section .banner-text .btn-outline-primary');
-    button.on('touchstart', function(){
+    button.on('touchstart', function () {
         $(this).addClass('hover');
     });
-    button.on('touchend', function(){
+    button.on('touchend', function () {
         $(this).removeClass('hover');
     });
 }
 
-function AnimationSkillCard(){
+function AnimationSkillCard() {
     let card = $('#skills-section .container .card');
-    card.on('touchstart', function(){
+    card.on('touchstart', function () {
         $(this).addClass('hover');
         $(this).find('h5').addClass('hover');
         $(this).find('span').addClass('hover');
     });
-    card.on('touchend', function(){
+    card.on('touchend', function () {
         $(this).removeClass('hover');
         $(this).find('h5').removeClass('hover');
         $(this).find('span').removeClass('hover');
     });
 }
 
+function MakeThreeFrameInCarousel() {
+    if ($(window).width() > 768) {
+        $('#carouselSchool .carousel-item').each(function () {
+            let next = $(this).next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+            next.children(':first-child').clone().appendTo($(this));
+            if (next.next().length > 0) {
+                next.next().children(':first-child').clone().appendTo($(this));
+            } else {
+                $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+            }
+        });
+    }
+}
 
-
-$(document).ready(function(){
+$(document).ready(function () {
     ScrollOffsetTop();
     StickyNav();
     AnimationDownloadCVButton();
     AnimationSkillCard();
+    MakeThreeFrameInCarousel();
 })
